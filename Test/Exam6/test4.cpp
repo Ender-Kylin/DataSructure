ee5664154b46752c1fb74df51bab1d07a6ce6bd4
Ender_Kylin <3162176860@qq.com>
2025-05-07 15:25:57 +0800

    第四题错误修正

--- Test/Exam6/test4.cpp
+++ Test/Exam6/test4.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #define MVNum 20
 #define MaxInt INT_MAX
 
-
 typedef char VertexType;
 typedef int ArcType;
 
@@ -17,15 +16,14 @@ typedef struct
     ArcType arcs[MVNum][MVNum];
     int vexnum;
     int arcnum;
-}AMGraph;
-
+} AMGraph;
 
 int LocateVex(AMGraph G, VertexType v)
 {
     int i = 0;
-    for(i = 0; G.vexs[i]; ++i)
+    for (i = 0; G.vexs[i]; ++i)
     {
-        if(v == G.vexs[i])
+        if (v == G.vexs[i])
         {
             break;
         }
@@ -35,48 +33,51 @@ int LocateVex(AMGraph G, VertexType v)
 
 void CreateUDN(AMGraph &G)
 {
-    cin >>G.arcnum>>G.vexnum;
-    for(int i = 0; i < G.vexnum; ++i)
+    cin >> G.arcnum >> G.vexnum;
+    for (int i = 0; i < G.vexnum; ++i)
     {
-        cin >>G.vexs[i];
+        cin >> G.vexs[i];
     }
-    for(int i = 0; i < G.vexnum; ++i)
+    for (int i = 0; i < G.vexnum; ++i)
     {
-        for(int  j = 0; j < G.vexnum; ++j)
+        for (int j = 0; j < G.vexnum; ++j)
         {
-            *(*(G.arcs + i)+j) = -1;  //初始化为-1
+            *(*(G.arcs + i) + j) = -1; // 初始化为-1
         }
     }
-    for(int k = 0; k < G.arcnum; ++k)
+    for (int k = 0; k < G.arcnum; ++k)
     {
-        VertexType v1 ,v2;
+        VertexType v1, v2;
         ArcType w;
-        cin >>v1>>v2>>w;
-        int i = LocateVex(G,v1), j = LocateVex (G, v2);
-        G.arcs[i][j] = G.arcs[j][i] =w;
+        cin >> v1 >> v2 >> w;
+        int i = LocateVex(G, v1), j = LocateVex(G, v2);
+        G.arcs[i][j] = G.arcs[j][i] = w;
     }
 }
 
 void BFS_AM(AMGraph G, int v)
 {
-    int * vexs_BFS = new int[G.vexnum];
+    int vexs_BFS[MVNum];
     bool visited[MVNum];
-    
+    for(int i = 0;i < G.vexnum;++i)
+    {
+        vexs_BFS[i] =0 ;
+        visited[i] = false;
+    }
     int p = 0, q = 0;
-    cout <<G.vexs[v];
+    cout << G.vexs[v]<<" ";
     vexs_BFS[q++] = v;
     visited[v] = true;
-    while(p != q)
+    while (p != q)
     {
         int u = p++;
-        for(int  i = G.vexnum; i > -1 ; --i)
+        for (int i = G.vexnum - 1; i > -1; --i)
         {
-            if(G.arcs[u][i] != -1 && !visited[i])
+            if (G.arcs[vexs_BFS[u]][i] != -1 && !(visited[i]))
             {
-                cout<<G.vexs[i]<<" ";
-                visited[i] = true
-                ;
-                vexs_BFS[q++] = v;
+                cout << G.vexs[i] << " ";
+                visited[i] = true;
+                vexs_BFS[q++] = i;
             }
         }
     }
